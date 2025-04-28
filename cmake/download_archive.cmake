a9d006956c795bd7cc07fba0a312ff50fd50f84d
Kene Lin <kenelin@gmail.com>
2025-04-28 18:13:45 +0800

    Disable archive downloading (#5)

--- cmake/download_archive.cmake
+++ cmake/download_archive.cmake
@@ -18,23 +18,23 @@ file(MAKE_DIRECTORY ${_download_archive_TEMPORARY_DIR})
 # This is basically Bazel's http_archive.
 # Note that strip_prefix strips the directory path prefix of the extracted
 # archive content, and it may strip multiple directories.
-function(download_archive destination url hash strip_prefix)
-  # Fetch and validate
-  set(_TEMPORARY_FILE ${_download_archive_TEMPORARY_DIR}/${strip_prefix}.tar.gz)
-  message(STATUS "Downloading from ${url}, if failed, please try configuring again")
-  file(DOWNLOAD ${url} ${_TEMPORARY_FILE}
-       TIMEOUT 60
-       EXPECTED_HASH SHA256=${hash}
-       TLS_VERIFY ON)
-  # Extract
-  execute_process(COMMAND
-                  ${CMAKE_COMMAND} -E tar xvf ${_TEMPORARY_FILE}
-                  WORKING_DIRECTORY ${_download_archive_TEMPORARY_DIR}
-                  OUTPUT_QUIET)
-  get_filename_component(_download_archive_Destination_Path ${destination} DIRECTORY)
-  file(MAKE_DIRECTORY ${_download_archive_Destination_Path})
-  file(RENAME ${_download_archive_TEMPORARY_DIR}/${strip_prefix} ${destination})
-  # Clean up
-  file(REMOVE ${_download_archive_TEMPORARY_DIR}/${strip_prefix})
-  file(REMOVE ${_TEMPORARY_FILE})
+function(download_archive destination url hash strip_prefix)#
+#  # Fetch and validate
+#  set(_TEMPORARY_FILE ${_download_archive_TEMPORARY_DIR}/${strip_prefix}.tar.gz)
+#  message(STATUS "Downloading from ${url}, if failed, please try configuring again")
+#  file(DOWNLOAD ${url} ${_TEMPORARY_FILE}
+#       TIMEOUT 60
+#       EXPECTED_HASH SHA256=${hash}
+#       TLS_VERIFY ON)
+#  # Extract
+#  execute_process(COMMAND
+#                  ${CMAKE_COMMAND} -E tar xvf ${_TEMPORARY_FILE}
+#                  WORKING_DIRECTORY ${_download_archive_TEMPORARY_DIR}
+#                  OUTPUT_QUIET)
+#  get_filename_component(_download_archive_Destination_Path ${destination} DIRECTORY)
+#  file(MAKE_DIRECTORY ${_download_archive_Destination_Path})
+#  file(RENAME ${_download_archive_TEMPORARY_DIR}/${strip_prefix} ${destination})
+#  # Clean up
+#  file(REMOVE ${_download_archive_TEMPORARY_DIR}/${strip_prefix})
+#  file(REMOVE ${_TEMPORARY_FILE})
 endfunction()
